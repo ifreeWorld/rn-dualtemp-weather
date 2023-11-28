d5cb317798f167f4f36ef0ce25bb7f37c6de5f2d
zilong.wang <zilong.wang@jiduauto.com>
2023-11-28 17:06:23 +0800

    fix: t

--- app.config.js
+++ app.config.js
@@ -1,10 +1,10 @@
 export default ({ config }) => {
   const extra = {
     ...config.extra,
-    weatherAPI: process.env.OPEN_WEATHER_API ?? '',
-    revGeoAPI: process.env.REVERSE_GEO_API ?? '',
+    weatherAPI: '604a4eee84b9e0fd136efb915c972163',
+    revGeoAPI: '8a67078ab41c42b89f3b315f92d3594f',
     eas: {
-      projectId: '',
+      projectId: '754ff626-feae-4ae7-b026-4a2f8414b8a0',
     },
   };
 

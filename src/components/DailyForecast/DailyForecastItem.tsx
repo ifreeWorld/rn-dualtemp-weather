e59c9489cd000f728ab0b2eb548295a72d6adeac
zilong.wang <zilong.wang@jiduauto.com>
2023-11-29 18:02:01 +0800

    fix: fix

--- src/components/DailyForecast/DailyForecastItem.tsx
+++ src/components/DailyForecast/DailyForecastItem.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { DailyEntity } from '../../types/WeatherTypes';
 import { DailyForecastItemStyles } from './DailyForecast.Styles';
 import Card, { CardStyleTypes } from '../Card/Card';
-import moment from 'moment'
+import moment from 'moment';
 import WeatherIcon, { IconSizeTypes } from '../WeatherIcon/WeatherIcon';
 import { displayWeatherIcon } from '../../utils/Images';
 import TempText, { TempTextStyleTypes } from '../TempText/TempText';
@@ -11,32 +11,51 @@ import DailyForecastExpanded from './DailyForecastExpanded';
 import DualTempText from '../TempText/DualTempText';
 
 type DailyForecastItemPropTypes = {
-    day: DailyEntity;
-    index: number;
-    currSelected: number;
-    setSelected: (index: number) => void;
-}
+  day: DailyEntity;
+  index: number;
+  currSelected: number;
+  setSelected: (index: number) => void;
+};
 
-const DailyForecastItem = ({ day, index, setSelected, currSelected }: DailyForecastItemPropTypes) => {
-    return (
-        <Card cardType={index === currSelected ? CardStyleTypes.DAILYXL : CardStyleTypes.DAILY}>
-            <TouchableOpacity onPress={() => setSelected(index)}>
-                <View style={DailyForecastItemStyles.dailyItemHeader}>
-                    <Text style={DailyForecastItemStyles.dayText}>{moment.unix(day.dt).format('dddd')}</Text>
-                    <View style={DailyForecastItemStyles.tempIconContainer}>
-                        <Text style={DailyForecastItemStyles.popText}>{(day.pop * 100).toFixed(0)}%</Text>
-                        <View style={DailyForecastItemStyles.tempContainer}>
-                            <DualTempText temp={day.temp.day} tempStyleC={TempTextStyleTypes.DAILY} degree divider />
-                        </View>
-                        <WeatherIcon icon={displayWeatherIcon(day.weather[0].icon)} iconSize={IconSizeTypes.SMALL} />
-                    </View>
-                </View>
-                {index === currSelected &&
-                    <DailyForecastExpanded day={day} />
-                }
-            </TouchableOpacity>
-        </Card>
-    )
-}
+const DailyForecastItem = ({
+  day,
+  index,
+  setSelected,
+  currSelected,
+}: DailyForecastItemPropTypes) => {
+  return (
+    <Card
+      cardType={
+        index === currSelected ? CardStyleTypes.DAILYXL : CardStyleTypes.DAILY
+      }
+    >
+      <TouchableOpacity onPress={() => setSelected(index)}>
+        <View style={DailyForecastItemStyles.dailyItemHeader}>
+          <Text style={DailyForecastItemStyles.dayText}>
+            {moment.unix(day.dt).format('dddd')}
+          </Text>
+          <View style={DailyForecastItemStyles.tempIconContainer}>
+            <Text style={DailyForecastItemStyles.popText}>
+              {(day.pop * 100).toFixed(0)}%
+            </Text>
+            <View style={DailyForecastItemStyles.tempContainer}>
+              <DualTempText
+                temp={day.temp.day}
+                tempStyleC={TempTextStyleTypes.DAILY}
+                degree
+                divider
+              />
+            </View>
+            <WeatherIcon
+              icon={displayWeatherIcon(day.weather[0].icon)}
+              iconSize={IconSizeTypes.SMALL}
+            />
+          </View>
+        </View>
+        {index === currSelected && <DailyForecastExpanded day={day} />}
+      </TouchableOpacity>
+    </Card>
+  );
+};
 
-export default DailyForecastItem
+export default DailyForecastItem;
--- src/components/TempText/DualTempText.tsx
+++ src/components/TempText/DualTempText.tsx
@@ -1,39 +1,47 @@
-import React, { useContext } from 'react'
-import { Text } from 'react-native'
-import { AppStateContext, AppStateProviderPropTypes } from '../../utils/AppStateContext'
-import { DailyForecastItemStyles } from '../DailyForecast/DailyForecast.Styles'
-import TempText, { TempTextStyleTypes } from './TempText'
+import React, { useContext } from 'react';
+import { Text } from 'react-native';
+import {
+  AppStateContext,
+  AppStateProviderPropTypes,
+} from '../../utils/AppStateContext';
+import { DailyForecastItemStyles } from '../DailyForecast/DailyForecast.Styles';
+import TempText, { TempTextStyleTypes } from './TempText';
 
 type DualTempTextProps = {
-    temp: number;
-    tempStyleC: TempTextStyleTypes;
-    tempStyleF?: TempTextStyleTypes;
-    degree?: boolean;
-    divider?: boolean;
-}
+  temp: number;
+  tempStyleC: TempTextStyleTypes;
+  tempStyleF?: TempTextStyleTypes;
+  degree?: boolean;
+  divider?: boolean;
+};
 
+const DualTempText = ({
+  temp,
+  tempStyleC,
+  tempStyleF = tempStyleC,
+  degree = false,
+  divider = false,
+}: DualTempTextProps) => {
+  const context = useContext(AppStateContext);
+  const tempScale = context?.tempScale;
 
-const DualTempText = ({ temp, tempStyleC, tempStyleF = tempStyleC, degree = false, divider = false }: DualTempTextProps) => {
-    const context = useContext(AppStateContext);  
-    const tempScale = context?.tempScale
+  return (
+    <>
+      <TempText
+        temp={temp}
+        textStyleType={tempStyleC}
+        tempType={tempScale}
+        withSym={degree}
+      />
+      {divider && <Text style={DailyForecastItemStyles.tempDivider}> | </Text>}
+      <TempText
+        temp={temp}
+        textStyleType={tempStyleF}
+        tempType={tempScale === 'C' ? 'F' : 'C'}
+        withSym={degree}
+      />
+    </>
+  );
+};
 
-    return (
-        <>
-            <TempText
-                temp={temp}
-                textStyleType={tempStyleC}
-                tempType={tempScale}
-                withSym={degree}
-            />
-            {divider && <Text style={DailyForecastItemStyles.tempDivider}> | </Text>}
-            <TempText
-                temp={temp}
-                textStyleType={tempStyleF}
-                tempType={tempScale === 'C' ? 'F' : 'C'}
-                withSym={degree}
-            />
-        </>
-    )
-}
-
-export default DualTempText
+export default DualTempText;
